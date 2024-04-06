473d9ea195154a0e1f8a81430a2cbfe81032b78d
ashok <165985264+ashok-scripts@users.noreply.github.com>
2024-04-06 19:23:59 +0530

    Update find_most_utilized.sh

--- find_most_utilized.sh
+++ find_most_utilized.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# NOTICE TO USERS
+# This script is designed to find the most utilized files on a remote host.
+# Please ensure that SSH is properly configured on both local and remote machines.
+# Use of sudo for SSH may be necessary depending on the permissions of the remote path.
+
 # Prompt user for remote hostname
 read -p "Enter remote hostname: " remote_host
 
@@ -17,9 +22,8 @@ else
 fi
 
 # Execute the remote command using SSH and capture output
-remote_cmd="$($ssh_command "$remote_host" "/usr/bin/find '$path' -xdev -ls | sort -rnk7 | head -10")"
+output="$($ssh_command "$remote_host" "/usr/bin/find '$path' -xdev -ls | sort -rnk7 | head -10")"
 
 # Display results
-output=$(eval "$remote_cmd")
 echo "Most utilized files on $remote_host in '$path':"
 echo "$output"
